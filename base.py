36ae31535ca8401c55cb798dcfb48e394b963f06
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-03 14:41:21 +1100

    updated ssh options

--- base.py
+++ base.py
@@ -20,22 +20,22 @@ class Connector(object):
 
         else:
             options = []
+            if self.username:
+                options.append('-l {0}'.format(self.username))
 
             if self.ignore_known_hosts:
-                options.append('-o UserKnownHostsFile=/dev/null/')
+                options.append('-o UserKnownHostsFile=/dev/null')
 
             if self.disable_host_key_checking:
                 options.append('-o StrictHostKeyChecking=no')
 
             if self.ssh_key_file:
-                options.append('-o {0}'.format(self.ssh_key_file))
+                options.append('-o IdentityFile={0}'.format(self.ssh_key_file))
 
             if self.ignore_ssh_config:
                 options.append('-F /dev/null')
 
-            self.ssh_driver = 'ssh {0} {1}@{2} -p {3}'.format(' '.join(options),
-                                                              self.username,
-                                                              self.device,
-                                                              self.port)
+            self.ssh_driver = 'ssh {0} {1}'.format(' '.join(options),
+                                                   self.device)
 
         self.telnet_driver = 'telnet {0} {1}'.format(self.device, self.port)
