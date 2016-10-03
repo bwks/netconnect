63f2be2addf82df5c11c384b7df91beb1355f596
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-03 15:46:44 +1100

    check unix expect parameters

--- login.py
+++ login.py
@@ -3,14 +3,14 @@ import pexpect
 
 def unix_login(connection, login_cmd):
     child = pexpect.spawn(login_cmd)
-    i = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*>', '.*#', '.*assword.*'])
+    i = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#', '.*$', '.*assword.*'])
     if i == (0 or 1):
         raise i
     elif i == (2 or 3):
         return child
     elif i == 4:
         child.sendline(connection.password)
-        j = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*>', '.*#'])
+        j = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#', '.*$'])
         if j == (0 or 1):
             raise i
         elif j == (2 or 3):
