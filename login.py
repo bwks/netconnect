f6ed198fcaf169384f12fc67e2efa63a84f34d6f
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-03 19:28:01 +1100

    framed out unix and cisco login

--- login.py
+++ login.py
@@ -1,7 +1,30 @@
 import pexpect
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
-def unix_login(connection, login_cmd):
+
+def validate_login_type(login_type):
+    if login_type.lower() not in ['ssh', 'telnet']:
+        raise ValueError('Invalid login type {0}. '
+                         'Valid types are ssh and telnet'.format(login_type))
+
+
+def unix_login(connection, login_type='ssh'):
+    """
+    Login to linux/unix shell (bash terminal assumed)
+    :param connection: Connector object
+    :param login_type: SSH or Telnet
+    :return: pexpect spawn object
+
+    Authentication types:
+     - username and password
+     - certificate based
+    """
+    validate_login_type(login_type)
+
+    login_cmd = connection.ssh_cmd if login_type.lower() is 'ssh' else connection.telnet_cmd
+
     child = pexpect.spawn(login_cmd)
     i = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#', '.*$', '.*assword.*'])
     if i == (0 or 1):
@@ -17,9 +40,53 @@ def unix_login(connection, login_cmd):
             return child
 
 
-def cisco_login():
-    pass
+def cisco_login(connection, login_type='ssh', enable_password=''):
+    """
+    Login to Cisco IOS, IOS-XE, NXOS
+    :param connection: Connector object
+    :param login_type: SSH or Telnet
+    :param enable_password: Enable password if required
+    :return: pexpect spawn object
 
+    Authentication types:
+     - username and password
+     - certificate based
+    """
+    validate_login_type(login_type)
+
+    login_cmd = connection.ssh_cmd if login_type.lower() is 'ssh' else connection.telnet_cmd
+
+    child = pexpect.spawn(login_cmd)
+    i = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#', '.*assword.*', '.*>'])
+    if i == (0 or 1):
+        raise i
+    elif i == 2:
+        return child
+    elif i == 3:
+        child.sendline(connection.password)
+        j = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#', '.*>'])
+        if j == (0 or 1):
+            raise i
+        elif j == 2:
+            return child
+        elif j == 3:
+            if not enable_password:
+                raise ValueError('Need enable password, but None provided')
+            child.sendline(enable_password)
+            k = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#'])
+            if k == (0 or 1):
+                raise k
+            elif k == 2:
+                return child
+    elif i == 4:
+        if not enable_password:
+            raise ValueError('Need enable password, but None provided')
+        child.sendline(enable_password)
+        j = child.expect([pexpect.EOF, pexpect.TIMEOUT, '.*#'])
+        if j == (0 or 1):
+            raise j
+        elif j == 2:
+            return child
 
 def juniper_login():
     pass
