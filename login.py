21f012675497a2cc4bcd30d34baebc1780e4123b
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-06 00:57:03 +1100

    refactored error clean up and mapped arista login to cisco login

--- login.py
+++ login.py
@@ -15,6 +15,12 @@ def validate_login_type(login_type):
                          'Valid types are ssh and telnet'.format(login_type))
 
 
+def clean_up_error(child, error):
+    helpers.debug_output(child)
+    child.close()
+    helpers.parse_error(error)
+
+
 def unix_login(connector, login_type='ssh'):
     """
     Login to linux/unix shell (bash terminal assumed)
@@ -64,18 +70,14 @@ def cisco_login(connector, login_type='ssh', enable_password=''):
     child = pexpect.spawn(login_cmd, timeout=connector.timeout)
     i = child.expect(PEXPECT_ERRORS + ['.*#', '.*assword', '.*>'])
     if i == (0 or 1):
-        helpers.debug_output(child)
-        child.kill(0)
-        helpers.parse_error(i)
+        clean_up_error(child, i)
     elif i == 2:
         return child
     elif i == 3:
         child.sendline(connector.password)
         j = child.expect(PEXPECT_ERRORS + ['.*#', '.*>'])
         if j == (0 or 1):
-            helpers.debug_output(child)
-            child.kill(0)
-            helpers.parse_error(i)
+            clean_up_error(child, j)
         elif j == 2:
             return child
         elif j == 3:
@@ -84,16 +86,12 @@ def cisco_login(connector, login_type='ssh', enable_password=''):
             child.sendline('enable')
             k = child.expect(PEXPECT_ERRORS + ['.*assword'])
             if k == (0 or 1):
-                helpers.debug_output(child)
-                child.kill(0)
-                helpers.parse_error(i)
+                clean_up_error(child, k)
             elif k == 2:
                 child.sendline(enable_password)
                 l = child.expect(PEXPECT_ERRORS + ['.*#'])
                 if l == (0 or 1):
-                    helpers.debug_output(child)
-                    child.kill(0)
-                    helpers.parse_error(i)
+                    clean_up_error(child, l)
                 elif l == 2:
                     return child
     elif i == 4:
@@ -110,5 +108,5 @@ def juniper_login():
     pass
 
 
-def arista_login():
-    pass
+def arista_login(connector, login_type='ssh', enable_password=''):
+    cisco_login(connector=connector, login_type=login_type, enable_password=enable_password)
