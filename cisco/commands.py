79739412c36e0a95af038fa6b4a9a1a661711a43
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-08 19:52:57 +1100

    refactored return and made prompt optional

--- cisco/commands.py
+++ cisco/commands.py
@@ -2,14 +2,15 @@ import logging
 from netconnect.helpers import get_prompt, clean_up_error, PEXPECT_ERRORS
 
 
-def disable_paging(child, prompt):
-    child.sendling('config paging disable')
-    i = child.expect(PEXPECT_ERRORS + [prompt])
+def disable_paging(child, prompt=''):
+    expect_prompt = prompt if prompt else '.*#'
+    child.sendline('terminal length 0')
+    i = child.expect(PEXPECT_ERRORS + [expect_prompt])
     if i == (0 or 1):
         clean_up_error(child, i)
     elif i == 2:
-        logging.debug('{0} paging disabled'.format(prompt))
-        return child
+        logging.debug('paging disabled')
+        return True
 
 
 def enable_iosex_api():
