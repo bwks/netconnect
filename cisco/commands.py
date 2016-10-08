185b73d1b84304b1350e023e9b37c09c7fbffaa4
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-10-08 19:23:40 +1100

    added function to disable_paging

--- cisco/commands.py
+++ cisco/commands.py
@@ -1,2 +1,16 @@
+import logging
+from netconnect.helpers import get_prompt, clean_up_error, PEXPECT_ERRORS
+
+
+def disable_paging(child, prompt):
+    child.sendling('config paging disable')
+    i = child.expect(PEXPECT_ERRORS + [prompt])
+    if i == (0 or 1):
+        clean_up_error(child, i)
+    elif i == 2:
+        logging.debug('{0} paging disabled'.format(prompt))
+        return child
+
+
 def enable_iosex_api():
     pass
