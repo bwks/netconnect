49ef06cbd78fe7d5289208e3693eef91dc242dcb
Brad Searle <brad.searle@nttict.com.au>
2016-11-10 23:14:14 +1100

    added juniper driver tests

---
+++ netconnect/juniper/tests/test_juniper_driver.py
@@ -0,0 +1,13 @@
+import pytest
+
+from netconnect.juniper.juniper_driver import JuniperDriver
+
+
+@pytest.fixture()
+def setup_juniper_driver():
+    dev = JuniperDriver(device='test-dev', username='test-user', password='password')
+    return dev
+
+
+def test_setup_juniper_driver_is_a_cisco_juniper_object(setup_juniper_driver):
+    assert isinstance(setup_juniper_driver, JuniperDriver)
