505728b91c050eecd4b3657bdc5bc735647650f7
Brad Searle <brad.searle@nttict.com.au>
2016-11-12 10:35:24 +1100

    added enable api method

--- netconnect/arista/arista_driver.py
+++ netconnect/arista/arista_driver.py
@@ -2,4 +2,18 @@ from netconnect.cisco.cisco_driver import CiscoDriver
 
 
 class AristaDriver(CiscoDriver):
-    pass
+    """
+    Driver to login and send commands to cisco devices.
+    """
+    def enable_api(self, https_port=443):
+        """
+        Enable device API.
+        :return:
+        """
+        commands = ['management api http-commands',
+                    'protocol https {0}'.format(https_port),
+                    'no shutdown']
+
+        self.configuration_mode()
+        self.send_commands(commands=commands, prompt='.*api-http-cmds\)#')
+        self.save_config()
