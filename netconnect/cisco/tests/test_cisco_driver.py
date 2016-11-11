6a54b81f6ed0ce23dbc73bf39f46aa1f4832d7d0
Brad Searle <brad.searle@nttict.com.au>
2016-11-11 21:20:57 +1100

    tests added

--- netconnect/cisco/tests/test_cisco_driver.py
+++ netconnect/cisco/tests/test_cisco_driver.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import patch, Mock
 
 from netconnect.cisco.cisco_driver import CiscoDriver
 
@@ -9,5 +10,15 @@ def setup_cisco_driver():
     return dev
 
 
+@patch.object(CiscoDriver, 'disable_paging')
+def fake_disable_paging(mock_disable_paging):
+    mock_disable_paging.return_value = True
+    return CiscoDriver.disable_paging()
+
+
 def test_setup_cisco_driver_is_a_cisco_driver_object(setup_cisco_driver):
     assert isinstance(setup_cisco_driver, CiscoDriver)
+
+
+def test_disable_paging_returns_true():
+    assert fake_disable_paging() is True
