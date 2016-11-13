c04fa560015558c7e370d283bbbb01fdb73d7bfe
Brad Searle <brad.searle@nttict.com.au>
2016-11-13 14:00:00 +1100

    added arista driver functional tests and updated comments in cisco func tests

--- netconnect/arista/tests/test_arista_driver_functional.py
+++ netconnect/arista/tests/test_arista_driver_functional.py
@@ -12,7 +12,7 @@ def setup_arista_driver():
     return dev
 
 
-@pytest.mark.skipif(not run_tests, reason='test requires cisco device')
+@pytest.mark.skipif(not run_tests, reason='test requires arista device')
 def test_enable_api_returns_true(setup_arista_driver):
     setup_arista_driver.login(enable_password='Password')
     assert setup_arista_driver.enable_api() is True
