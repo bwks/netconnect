1d8842ebce852dae2506cf6256ac4ac0e0b086da
Brad Searle <bobthebutcher@users.noreply.github.com>
2017-01-03 21:35:46 +1100

    Issue 13 (#17)
    * modified split string
    
    * added aditional split string
    
    * added test for prompt
    
    * initial commit
    
    * adjusting timing on save method to compensate for asa
    
    * added python 2.7 version
    
    * custom exceptions
    
    * customize exceptions
    
    * added mock library install for python 2.7
    
    * account for python 2 and added mock tests
    
    * account for python 2/3
    
    * option to get pexpect EOF or TIMEOUT erro
    
    * use custom exceptions
    
    * updated comments
    
    * refactored use netconnect exceptions
    
    * refactored disable paging to helper file
    
    * refactored to use netconnect exceptions
    
    * refactoring asa driver

--- netconnect/cisco/cisco_asa_driver.py
+++ netconnect/cisco/cisco_asa_driver.py
@@ -1,3 +1,4 @@
+import time
 import logging
 
 from netconnect.helpers import (
@@ -24,6 +25,10 @@ class CiscoASADriver(CiscoDriver):
             prompt = self.get_prompt()
 
         self.child.sendline('terminal pager 0')
+        # ASA has a timing issue when saving config. Adding
+        # in 1 second of sleep before expecting prompt to compensate
+        time.sleep(1)
+
         i = self.child.expect(PEXPECT_ERRORS + [prompt])
         if i == 0 or i == 1:
             logging.debug('{0} error sending disable paging command'.format(self.device))
@@ -31,3 +36,69 @@ class CiscoASADriver(CiscoDriver):
         elif i == 2:
             logging.debug('{0} paging disabled'.format(self.device))
             return True
+
+    def enable_scp(self):
+        """
+        Enable SCP to facilitate secure file transfer to device
+        :return: True if successful
+        """
+        self.configuration_mode()
+        self.child.sendline('ssh scopy enable')
+        i = self.child.expect(PEXPECT_ERRORS + ['.*\(config\)#'])
+        if i == 0 or i == 1:
+            logging.debug('{0} error sending scp command'.format(self.device))
+            clean_up_error(self.child, i)
+        elif i == 2:
+            logging.debug('{0} scp enabled'.format(self.device))
+            return True
+
+    def add_service_route(self, service, subnet, subnet_mask, interface_name):
+        """
+        Add a service route for example:
+          - http 0.0.0.0 0.0.0.0 management
+        :param service: Name of service
+        :param subnet: Permitted subnet
+        :param subnet_mask: Permitted subnet mask
+        :param interface_name: Name of interface
+        :return: True is successful
+        """
+        route = '{0} {1} {2} {3}'.format(service, subnet, subnet_mask, interface_name)
+        pass
+
+    def add_static_route(self, interface_name, subnet, subnet_mask, next_hop):
+        """
+        Add a static route for example:
+          - route management 0.0.0.0 0.0.0.0 10.1.1.1 1
+        :param interface_name: Name of interface
+        :param subnet: Destination subnet
+        :param subnet_mask: Destination subnet mask
+        :param next_hop: Next hop IP address
+        :return: True if successful
+        """
+        route = 'route management 0.0.0.0 0.0.0.0 10.1.1.1 1'
+        pass
+
+    def enable_api(self, port=443):
+        """
+        Enable ASA API. Must have rest API extensions installed on flash
+        :param port: API port number
+        :return: True if successful
+        """
+        self.configuration_mode()
+        self.child.sendline('http server enable {0}'.format(port))
+        i = self.child.expect(PEXPECT_ERRORS + ['.*\(config\)#'])
+        if i == 0 or i == 1:
+            logging.debug('{0} error sending http server command'.format(self.device))
+            clean_up_error(self.child, i)
+        elif i == 2:
+            self.child.sendline('rest-api agent')
+            j = self.child.expect(PEXPECT_ERRORS + ['.*\(config\)#'])
+            if j == 0 or j == 1:
+                logging.debug('{0} error rest-api agent command'.format(self.device))
+                clean_up_error(self.child, j)
+            elif j == 2:
+                if 'error' not in j.child.before:
+                    logging.debug('{0} rest-api enabled'.format(self.device))
+                    return True
+                else:
+                    return False
